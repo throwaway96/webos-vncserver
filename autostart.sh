throwaway96/webos-vncserver

commit 39c15e2f304f0aa05c8a41d2e1221a9eb207799c

autostart: fix webOS 7 autostart

autostart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-timeout -t 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'
+timeout 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'

commit 7c0730a7013b22269fbbf4d02323a06437e2ff22

fix autostart compatibility
Commit 39c15e2 broke the startup script on webOS <7.

The Busybox timeout requires different arguments on old and new webOS.
Until webOS 6 (v1.29.3), the timeout duration must be specified with -t.
As of webOS 7 (v1.31.1), -t is no longer accepted.

This script tries to run timeout with -t first. If that fails, it checks
the exit status. A status of 143 indicates that the process was killed
with SIGTERM, which in this case (probably) means it timed out. If the
command failed for any other reason (such as not accepting -t), try it
again without -t.

autostart.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-timeout 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'
+timeout -t 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}' || \
+  test "$?" -eq 143 || timeout 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'