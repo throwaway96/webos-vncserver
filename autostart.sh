6b80393344b798dd40b8bd67e985d5d238977c72
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2023-12-05 20:31:31 -0500

    fix autostart compatibility
    Commit 39c15e2 broke the startup script on webOS <7.
    
    The Busybox timeout requires different arguments on old and new webOS.
    Until webOS 6 (v1.29.3), the timeout duration must be specified with -t.
    As of webOS 7 (v1.31.1), -t is no longer accepted.
    
    This script tries to run timeout with -t first. If that fails, it checks
    the exit status. A status of 143 indicates that the process was killed
    with SIGTERM, which in this case (probably) means it timed out. If the
    command failed for any other reason (such as not accepting -t), try it
    again without -t.
    
    This is a shorter version that puts the command in a variable.

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-timeout 5 luna-send -n 1 'luna://org.webosbrew.vncserver.service/start' '{"autostart": true}'
+command='luna-send -n 1 luna://org.webosbrew.vncserver.service/start {"autostart":true}'
+timeout -t 5 $command || [ $? -eq 143 ] || timeout 5 $command
